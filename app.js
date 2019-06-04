9a0c5acd14022afcb1bdf8e7c60ac109daec9e52
Benjamin Black <benjamin.black@turner.com>
2019-06-04 13:19:17 -0400

    wip

--- app.js
+++ app.js
@@ -1,54 +1,42 @@
 /* global math */
 
+const defaultDistanceBetweenTrees = 15.0;
+const defaultRidgelineLength = 108.0;
+const defaultSitHeight = 18.0;
+const defaultHangAngle = 30.0;
+
 const distanceBetweenTreesInput = document.querySelector('#dt');
 const ridgelineLengthInput = document.querySelector('#len');
 const sitHeightInput = document.querySelector('#sit');
 const hangAngleInput = document.querySelector('#angle');
 const weightInput = document.querySelector('#weight');
-
-function cm(inches) {
-  return math.unit(inches, 'in').toNumber('cm');
-}
-
-function feet(meters) {
-  return math.unit(meters, 'm').toNumber('ft');
-}
-
-function inches(cm) {
-  return math.unit(cm, 'cm').toNumber('in');
-}
-
-function lbs(kilo) {
-  return math.unit(kilo, 'kg').toNumber('lb');
-}
-
-function kilo(lbs) {
-  return math.unit(lbs, 'lb').toNumber('kg');
-}
+const ridgelineLengthSelector = document.querySelector('#list');
 
 function getUnitsType() {
-  const type = document.querySelector('input[type="radio"][name="units"]:checked').value;
+  const type = +document.querySelector('input[type="radio"][name="units"]:checked').value;
 
   if (type === 0) {
-    return 'Metric';
-  } else {
     return 'English';
+  } else {
+    return 'Metric';
   }
 }
 
 function setEnglish() {
-  distanceBetweenTreesInput.value = feet(distanceBetweenTreesInput.value);
-  ridgelineLengthInput.value = inches(ridgelineLengthInput.value);
-  sitHeightInput.value = inches(sitHeightInput.value);
-  weightInput.value = lbs(weightInput.value);
+  distanceBetweenTreesInput.value = math.unit(+distanceBetweenTreesInput.value, 'm').toNumber('ft');
+  ridgelineLengthInput.value = math.unit(+ridgelineLengthInput.value, 'cm').toNumber('in');
+  sitHeightInput.value = math.unit(+sitHeightInput.value, 'cm').toNumber('in');
+  weightInput.value = math.unit(+weightInput.value, 'kg').toNumber('lb');
+
   hang();
 }
 
 function setMetric() {
   distanceBetweenTreesInput.value = math.unit(+distanceBetweenTreesInput.value, 'ft').toNumber('m');
-  ridgelineLengthInput.value = cm(+ridgelineLengthInput.value);
-  sitHeightInput.value = cm(+sitHeightInput.value);
-  weightInput.value = kilo(+weightInput.value);
+  ridgelineLengthInput.value = math.unit(+ridgelineLengthInput.value, 'in').toNumber('cm');
+  sitHeightInput.value = math.unit(+sitHeightInput.value, 'in').toNumber('cm');
+  weightInput.value = math.unit(+weightInput.value, 'lb').toNumber('kg');
+
   hang();
 }
 
@@ -67,59 +55,51 @@ function tension() {
 }
 
 function hang() {
-  var defaultDistanceBetweenTrees = 15.0;
-  var defaultRidgelineLength = 108.0;
-  var defaultSitHeight = 18.0;
-  var defaultHangAngle = 30.0;
+  const weight = +weightInput.value;
+  const units = getUnitsType();
 
-  var weight = +weightInput.value;
+  hangAngleInput.value = hangAngleInput.value || defaultHangAngle;
 
-  if (getUnitsType() === 'English') {
+  if (units === 'English') {
     distanceBetweenTreesInput.value = distanceBetweenTreesInput.value || defaultDistanceBetweenTrees;
     ridgelineLengthInput.value = ridgelineLengthInput.value || defaultRidgelineLength;
     sitHeightInput.value = sitHeightInput.value || defaultSitHeight;
   } else {
-    distanceBetweenTreesInput.value = distanceBetweenTreesInput.value || meters(defaultDistanceBetweenTrees);
-    ridgelineLengthInput.value = ridgelineLengthInput.value || cm(defaultRidgelineLength);
-    sitHeightInput.value = sitHeightInput.value || cm(defaultSitHeight);
+    distanceBetweenTreesInput.value = distanceBetweenTreesInput.value || math.unit(defaultDistanceBetweenTrees, 'ft').toNumber('m');
+    ridgelineLengthInput.value = ridgelineLengthInput.value || math.unit(defaultRidgelineLength, 'in').toNumber('cm');
+    sitHeightInput.value = sitHeightInput.value || math.unit(defaultSitHeight, 'in').toNumber('cm');
   }
 
-  hangAngleInput.value = hangAngleInput.value || defaultHangAngle;
+  var d123 = +distanceBetweenTreesInput.value;
 
-  var distanceBetweenTrees_len = +distanceBetweenTreesInput.value;
-  var distanceBetweenTreess;
+  let ridgelineLength = +ridgelineLengthInput.value;
 
-  var rl = +ridgelineLengthInput.value;
-
-  if ($('#list').val() == 1) {
-    rl = rl * Math.cos((hangAngleInput.value * Math.PI) / 180);
+  if (ridgelineLengthSelector.options[ridgelineLengthSelector.selectedIndex].value === 'hammock') {
+    ridgelineLength *= Math.cos(+hangAngleInput.value * (Math.PI / 180));
   }
 
-  var sitHeight = +sitHeightInput.value;
+  const sitHeight = +sitHeightInput.value;
 
-  if (getUnitsType() === 'English') {
-    distanceBetweenTreess = 12.0 * distanceBetweenTrees_len;
-  } else {
-    distanceBetweenTreess = 100.0 * distanceBetweenTrees_len;
-  }
-  var distanceBetweenTrees_height = Math.tan((hangAngleInput.value * Math.PI) / 180) * (distanceBetweenTreess / 2) + sitHeight;
-  var sp = (distanceBetweenTreess - rl) / 2 / Math.cos((hangAngleInput.value * Math.PI) / 180);
+  const distanceBetweenTrees = units === 'English' ? math.unit(d123, 'ft').toNumber('in') : math.unit(d123, 'm').toNumber('cm');
+
+  var distanceBetweenTrees_height = Math.tan((hangAngleInput.value * Math.PI) / 180) * (distanceBetweenTrees / 2) + sitHeight;
+  var sp = (distanceBetweenTrees - ridgelineLength) / 2 / Math.cos((hangAngleInput.value * Math.PI) / 180);
 
   if (getUnitsType() === 'Metric') {
-    $('#ridgeline span').html(rl.toFixed(1) + ' cm');
+    $('#ridgeline span').html(ridgelineLength.toFixed(1) + ' cm');
     $('#suspension-length span').html(sp.toFixed(1) + ' cm');
     $('#hang-height span').html(distanceBetweenTrees_height.toFixed(1) + ' cm');
     $('#sit-height span').html(sitHeight + ' cm');
-    $('#tree-distance span').html(distanceBetweenTrees_len + ' m');
+    $('#tree-distance span').html(d123 + ' m');
     $('#tensile span').html(tension() + ' kg');
     $('#shear span').html(shear() + ' kg');
     $('#hang-weight span').html(weight + ' kg');
   } else {
-    $('#ridgeline span').html(rl.toFixed(0) + ' in');
+    $('#ridgeline span').html(ridgelineLength.toFixed(0) + ' in');
     $('#suspension-length span').html(sp.toFixed(1) + ' in');
     $('#hang-height span').html(distanceBetweenTrees_height.toFixed(1) + ' in');
     $('#sit-height span').html(sitHeight + ' in');
-    $('#tree-distance span').html(distanceBetweenTrees_len + ' ft');
+    $('#tree-distance span').html(d123 + ' ft');
     $('#tensile span').html(tension() + ' lbs');
     $('#shear span').html(shear() + ' lbs');
     $('#hang-weight span').html(weight + ' lbs');
