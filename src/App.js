b330f4afaedefb03103b6c88bb49e7897b456455
Benjamin Black <benjamin@benjaminblack.net>
2019-08-30 01:40:16 -0400

    in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import hangCalculatorGraphic from './hang-calculator.jpg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="calc-app">
+      <img className="calc-graphic" src={hangCalculatorGraphic} alt="Hammock Hang Calculator"/>
+      <form className="calc-form">
+        <div className="units-imperial">
+          <input type="radio" name="input-units-imperial" checked={true}/> 
+          <label for="input-units-imperial">Imperial</label>
+        </div>
+        <div className="units-metric">
+          <input type="radio" name="input-units-metric" checked={false}/>
+          <label for="radio-units-metric">Metric</label>
+        </div>
+
+        <label className="label-distance-between-trees" for="input-distance-between-trees">Distance between trees</label>
+        <div className="distance-between-trees">
+          <input type="text" name="input-distance-between-trees" value="15"/>
+          <span class="units">ft</span>
+        </div>
+
+        <label className="label-preferred-sit-height" for="input-preferred-sit-height">Preferred sit height</label>
+        <div className="preferred-sit-height">
+          <input type="text" name="input-preferred-sit-height" value="18"/>
+          <span class="units">in</span>
+        </div>
+
+        <label className="label-weight-in-hammock" for="input-weight-in-hammock">Weight in hammock</label>
+        <div className="weight-in-hammock">
+          <input type="text" name="input-weight-in-hammock" value="200"/>
+          <span class="units">lbs</span>
+        </div>
+
+      </form>
+    </main>
   );
-}
+};
 
 export default App;
