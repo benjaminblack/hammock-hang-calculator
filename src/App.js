021ae8af92f016d19f64ede22d70507be1ee6b33
Benjamin Black <benjamin@benjaminblack.net>
2019-08-30 18:16:22 -0400

    move form into its own component and implement state management

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import hangCalculatorGraphic from './hang-calculator.jpg';
 import './App.css';
+import HangForm from './HangForm';
+import { UNITS_IMPERIAL, DEFAULT_DISTANCE_BETWEEN_TREES, DEFAULT_LENGTH_CHOICE, DEFAULT_SIT_HEIGHT, DEFAULT_WEIGHT, DEFAULT_HANG_ANGLE, DEFAULT_LENGTH } from './constants';
 
 const App = () => {
+  const [units, setUnits] = useState(UNITS_IMPERIAL);
+  const [distanceBetweenTrees, setDistanceBetweenTrees] = useState(DEFAULT_DISTANCE_BETWEEN_TREES);
+  const [lengthChoice, setLengthChoice] = useState(DEFAULT_LENGTH_CHOICE);
+  const [length, setLength] = useState(DEFAULT_LENGTH);
+  const [sitHeight, setSitHeight] = useState(DEFAULT_SIT_HEIGHT);
+  const [weight, setWeight] = useState(DEFAULT_WEIGHT);
+  const [hangAngle, setHangAngle] = useState(DEFAULT_HANG_ANGLE);
+
+  const formProps = {
+    units, setUnits,
+    distanceBetweenTrees, setDistanceBetweenTrees,
+    lengthChoice, setLengthChoice,
+    length, setLength,
+    sitHeight, setSitHeight,
+    weight, setWeight,
+    hangAngle, setHangAngle,
+  };
+
   return (
-    <main className="calc-app">
-      <img className="calc-graphic" src={hangCalculatorGraphic} alt="Hammock Hang Calculator"/>
-      <form className="calc-form">
-        <div className="units-imperial">
-          <input type="radio" name="input-units-imperial" checked={true}/> 
-          <label for="input-units-imperial">Imperial</label>
-        </div>
-        <div className="units-metric">
-          <input type="radio" name="input-units-metric" checked={false}/>
-          <label for="radio-units-metric">Metric</label>
-        </div>
-
-        <label className="label-distance-between-trees" for="input-distance-between-trees">Distance between trees</label>
-        <div className="distance-between-trees">
-          <input type="text" name="input-distance-between-trees" value="15"/>
-          <span class="units">ft</span>
-        </div>
-
-        <select className="select-ridgeline-or-hammock-length" value="ridgeline-length">
-          <option value="ridgeline-length">Ridgeline length</option>
-          <option value="hammock-length">Hammock length</option>
-        </select>
-        <div className="ridgeline-or-hammock-length">
-          <input type="text" name="input-ridgeline-or-hammock-length" value="108"/>
-          <span class="units">in</span>
-        </div>
-
-        <label className="label-preferred-sit-height" for="input-preferred-sit-height">Preferred sit height</label>
-        <div className="preferred-sit-height">
-          <input type="text" name="input-preferred-sit-height" value="18"/>
-          <span class="units">in</span>
-        </div>
-
-        <label className="label-weight-in-hammock" for="input-weight-in-hammock">Weight in hammock</label>
-        <div className="weight-in-hammock">
-          <input type="text" name="input-weight-in-hammock" value="200"/>
-          <span class="units">lbs</span>
-        </div>
-
-        <label className="label-hang-angle" for="input-hang-angle">Hang angle</label>
-        <div className="hang-angle">
-          <select name="input-hang-angle" value="30">
-            <option value="5">5</option>
-            <option value="15">15</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-            <option value="45">45</option>
-          </select>
-          <span class="units">&deg;</span>
-        </div>
-
-      </form>
+    <main className="hang-app">
+      <img className="hang-graphic" src={hangCalculatorGraphic} alt="Hammock Hang Calculator"/>
+      <HangForm {...formProps}/>
     </main>
   );
 };
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const UNITS_IMPERIAL = 'Imperial';
+export const UNITS_METRIC = 'Metric';
+export const LENGTH_CHOICE_RIDGELINE = 'Ridgeline length';
+export const LENGTH_CHOICE_HAMMOCK = 'Hammock length';
+export const HANG_ANGLE_CHOICES = [5, 15, 20, 30, 45];
+
+export const DEFAULT_DISTANCE_BETWEEN_TREES = 15;
+export const DEFAULT_LENGTH_CHOICE = LENGTH_CHOICE_RIDGELINE;
+export const DEFAULT_LENGTH = 108;
+export const DEFAULT_SIT_HEIGHT = 18;
+export const DEFAULT_WEIGHT = 200;
+export const DEFAULT_HANG_ANGLE = HANG_ANGLE_CHOICES[3];
