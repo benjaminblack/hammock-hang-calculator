0e1a9d9c6de4d1862b184e474bbbba901a48a5ce
Benjamin Black <benjamin@benjaminblack.net>
2019-08-30 22:57:26 -0400

    fix test script

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HangApp from './HangApp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<HangApp/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
